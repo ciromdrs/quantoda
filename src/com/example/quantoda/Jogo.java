fa3ecc5f5b0d1ab7a6a2d8c8f0a61d6db095ce60
Ciro Morais <ciromoraismedeiros@gmail.com>
2013-11-23 17:36:29 -0300

    Alterações na interface

---
+++ src/com/example/quantoda/Jogo.java
@@ -0,0 +1,27 @@
+package com.example.quantoda;
+
+public class Jogo {
+	private int nivel = 1;
+	
+	public void gerarNivel(int novo){
+		
+	}
+	
+	public boolean testarFimDeJogo(){
+		return true;
+	}
+	
+	public void proximoNivel(){
+		
+	}
+
+	public int getNivel() {
+		return nivel;
+	}
+
+	public void setNivel(int nivel) {
+		this.nivel = nivel;
+	}
+	
+	
+}
--- src/com/example/quantoda/MainActivity.java
+++ src/com/example/quantoda/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.quantoda;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 
 public class MainActivity extends Activity {
 
@@ -12,11 +13,43 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	/**
+	 * Método chamado ao clicar no botão sair. 
+	 */
+	public void sair(View v) {
+		finish();
+	}
+	
+	/**
+	 * Método chamado ao clicar no botão jogar. 
+	 */
+	public void jogar(View v) {
+		// Este é o frame que contém o fragment quando estiver num Tablet
+		FrameLayout frame = (FrameLayout) findViewById(R.id.frame_grande);
+		boolean telaPequena = frame == null;
+
+		int dificuldade = 0; // Inicia o jogo no nivel 0 para somar 1 a cada iteração
+		boolean perdeu = true;
+		
+		do {
+			dificuldade += 1;
+			// frag = Jogo.gerarNivel(dificuldade, telaPequena);
+			if (telaPequena){
+				// troca o frag pequeno
+			}else{
+				// coloca o frag grande
+			}
+			// perdeu = Resultado do jogo
+		} while (!perdeu);
+
+		// mostrar pontuções
+	}
+	
+	/**
+	 * Método chamado ao clicar no botão Ver Pontuações. 
+	 */
+	public void verPontuacoes(View v) {
+
 	}
 
 }
